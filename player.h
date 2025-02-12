18097465115612623c70d20699cdf460d075a2a6
SmallSigh <martijnsmit4@gmail.com>
2025-02-12 22:39:20 +0100

    player tweaks

---
+++ player.h
@@ -0,0 +1,24 @@
+typedef struct s_player
+{
+    char *name;
+    int strength;
+    int magic;
+    int turn;
+    int health;
+}       t_player;
+
+typedef struct s_wizard
+{
+    char *name;
+    int strength;
+    int magic;
+    int health;
+}       t_wizard;
+
+typedef struct s_orc
+{
+    char *name;
+    int strength;
+    int magic;
+    int health;
+}       t_orc;
