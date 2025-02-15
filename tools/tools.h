3df0a2b7a09192ed30a6a468f35a5c17b3ab5820
TerraMint <studentbreus@gmail.com>
2025-02-16 00:54:45 +0100

    Added mainscreen (though still named main.c) that presents the user with a small monologue

---
+++ tools/tools.h
@@ -0,0 +1,8 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include "../inc/GNL/get_next_line.h"
